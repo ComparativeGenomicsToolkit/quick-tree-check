0c112f951c0df93eec4e58e465758c5d003dce79
Roberto <roberto.r.gorbea@gmail.com>
2017-07-21 16:13:36 -0700

    I added a func that lists ancestors

--- least_squares_fit.py
+++ least_squares_fit.py
@@ -8,6 +8,9 @@ def parse_args():
 	parser.add_argument('inputTree', help='newick format tree (in a file)')
 	return parser.parse_args()
 
+
+
+
 def post_order(tree):
 	"""Return a list of nodes in tree in post-order."""
 	ret = []
@@ -22,6 +25,9 @@ def pre_order(tree):
 		ret.extend(pre_order(child))	
 	return ret
 
+
+
+
 def list_dict(list):
 	dict = {}
 	branch_val = 0
@@ -30,32 +36,69 @@ def list_dict(list):
 		branch_val += 1
 	return dict
 
-def scan(list):
+def scroll(list):
+	for i in range(len(list)-1):
+		for j in range(i+1,len(list)):
+			print list[i],list[j]
+
+
+
+
+def scan_leaves(list):
 	ret = []
 	for i in range(len(list)):
 		if list[i].is_leaf == True:
 			ret.append(list[i])
 	return ret
 
+def all_ancestors(node):
+	anc = []
+	current_node = node
+	while current_node.ancestor != None:
+		anc.append(current_node.ancestor)
+		current_node = current_node.ancestor
+	return anc
+
+def ancestor_list(list):
+	ret = []
+	i=1
+	leaves = scan_leaves(list)
+	for i in range (i,len(leaves)):
+		ret.append(all_ancestors(leaves[i]))
+	return ret
+
+
+
+
 def main():
 	opts = parse_args()
 	with open(opts.inputTree) as f:
-		trees = newick.load(f)
+		tree = newick.load(f)
 
-	for tree in trees:
-		print tree.ascii_art()
-		po = post_order(tree)
-		pre = pre_order(tree)
+	
+
+	for node in tree:
+		po = post_order(node)
+		pre = pre_order(node)
 		pre_dict = list_dict(pre)
+		print node.ascii_art()
 		print po
-	for node in po:
+		ancA = ancestor_list(po)
+		print "ancA is: %s" % ancA
+	
+		
+
+	"""for node in po:
 		print node.is_leaf
-	print scan(po)
+	for node in po:
+		print node.ancestor
+	print scan(po)"""
 
 
 
-	print 'trees is %s' % trees
-	print newick.dumps(trees)
+
+	print 'trees is %s' % tree
+	print newick.dumps(tree)
 if __name__ == "__main__":
 	main()
 
